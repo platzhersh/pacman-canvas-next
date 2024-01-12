8839904242400b0b472aa0efe27348403285df57
Christian Glatthard <christian.glatthard@cistec.com>
2024-01-12 20:54:32 +0100

    feat: add Direction class and implementations

---
+++ packages/pacman-canvas/src/figures/directions/Direction.ts
@@ -0,0 +1,28 @@
+export type DirectionName = "up" | "right" | "down" | "left";
+export type DirectionVelocity = -1 | 0 | 1;
+
+export class Direction {
+  private name: DirectionName;
+  private angle1: number;
+  private angle2: number;
+  private dirX: DirectionVelocity;
+  private dirY: DirectionVelocity;
+
+  constructor(
+    name: DirectionName,
+    angle1: number,
+    angle2: number,
+    dirX: DirectionVelocity,
+    dirY: DirectionVelocity
+  ) {
+    this.name = name;
+    this.angle1 = angle1;
+    this.angle2 = angle2;
+    this.dirX = dirX;
+    this.dirY = dirY;
+  }
+
+  public equals = (dir: Direction) => {
+    return JSON.stringify(this) == JSON.stringify(dir);
+  };
+}
