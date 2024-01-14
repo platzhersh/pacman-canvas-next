41199e972339f665b27bda4f3e02b7c3e2ad311d
Christian Glatthard <christian.glatthard@cistec.com>
2024-01-14 19:43:47 +0100

    feat: add button to add fruit

--- apps/web/app/components/gameCanvas.tsx
+++ apps/web/app/components/gameCanvas.tsx
@@ -137,26 +137,41 @@ export default function GameCanvas() {
             title="Inky"
             alt="inkySvg"
           />
-          <img onClick={() =>
+          <img
+            onClick={() =>
               game.getGhosts().blinky.toggleDirectionOptionsVisualizations()
-            } src={blinkySvgSrc} title="Blinky" alt="blinkySvg" />
-          <img onClick={() =>
+            }
+            src={blinkySvgSrc}
+            title="Blinky"
+            alt="blinkySvg"
+          />
+          <img
+            onClick={() =>
               game.getGhosts().pinky.toggleDirectionOptionsVisualizations()
-            } src={pinkySvgSrc} title="Pinky" alt="pinkySvg" />
-          <img onClick={() =>
+            }
+            src={pinkySvgSrc}
+            title="Pinky"
+            alt="pinkySvg"
+          />
+          <img
+            onClick={() =>
               game.getGhosts().clyde.toggleDirectionOptionsVisualizations()
-            } src={clydeSvgSrc} title="Clyde" alt="clydeSvg" />
+            }
+            src={clydeSvgSrc}
+            title="Clyde"
+            alt="clydeSvg"
+          />
           <img src={deadSvgSrc} title="Dead" alt="deadSvgSrc" />
           <img src={dazzledSvgSrc} title="Dazzled" alt="dazzledSvgSrc" />
           <img src={dazzled2SvgSrc} title="Dazzled2" alt="dazzled2SvgSrc" />
-          <img onClick={() => {
-                const foodHandler = new FoodHandler();
-                if (canvasContext) {
-                    foodHandler.magic(canvasContext, 200, 200);
-                }
-                
-          }} src={cherriesSvgSrc} title="cherries" alt="cherries" width="60" height="60"/>
-
+          <img
+            onClick={() => game.setMapContent(9, 9, "🍒")}
+            src={cherriesSvgSrc}
+            title="cherries"
+            alt="cherries"
+            width="60"
+            height="60"
+          />
         </section>
 
         <section>
